21c67625ff17ce1de6979480a5b4b92790ff64c6
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2015-10-15 21:03:14 +0300

    Initial commit

---
+++ Example/SGBackgroundView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SGBackgroundView
+//
+//  Created by Spiros Gerokostas on 10/15/2015.
+//  Copyright (c) 2015 Spiros Gerokostas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
